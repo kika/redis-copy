8f315ca8083a2275a8973ec26d2d3422501102a0
Kirill Pertsev <kirill.pertsev@upstart.com>
2019-05-15 14:31:04 -0700

    Fixed SCAN command return type and retab to 2 spaces

--- src/copy.ts
+++ src/copy.ts
@@ -4,8 +4,8 @@ const redis_url_from = process.argv[2]
 const redis_url_to = process.argv[3]
 if(!redis_url_from || redis_url_from.length === 0 || 
    !redis_url_to || redis_url_to.length === 0) {
-    console.error(`Usage: ${process.argv[1]} redis-url-from redis-url-to`)
-    process.exit(1);
+  console.error(`Usage: ${process.argv[1]} redis-url-from redis-url-to`)
+  process.exit(1);
 }
 
 const redis_from = redis.createHandyClient(redis_url_from, {return_buffers: true})
@@ -14,92 +14,90 @@ const redis_to = redis.createHandyClient(redis_url_to, {return_buffers: true})
 const batch_size = 10000
 const ignore_dupes = true
 
-type ScanResult = [number, string[]]
+type ScanResult = [string, string[]]
 
 copy_data(redis_from, redis_to)
-    .then(() => console.log("Done"))
-    .catch((err) => console.error(err))
+  .then(() => console.log("Done"))
+  .catch((err) => {
+    console.error(err)
+    process.exitCode = -1;
+  })
 
 async function copy_data(from: redis.IHandyRedis, to: redis.IHandyRedis) {
-    
-    let cursor = 0
-    let keyCount = 0
-    let dumpCount = 0
-    let dumpErrCount = 0
-    let restoredCount = 0
-    let restoreErrCount = 0
-    let ttlCount = 0
-    let ttlErrCount = 0
-    let skippedCount = 0
+  
+  let cursor: number = 0
+  let keyCount: number = 0
+  let readCount: number = 0
+  let restoredCount: number = 0
+  let restoreErrCount: number = 0
+  let readErrCount: number = 0
+  let skippedCount: number = 0
 
-    const stime = Date.now()
-    let message = ""
+  const stime = Date.now()
+  let message = ""
 
-    const totalKeys = await from.dbsize()
-    console.log(`Total keys: ${totalKeys}`)
+  const totalKeys = await from.dbsize()
+  console.log(`Total keys: ${totalKeys}`)
 
-    do {
-        const result: ScanResult = await from.scan(cursor, ['COUNT', batch_size])
-        cursor = result[0]
-        const keys = result[1]
-        keys.forEach((key) => {
-            keyCount++
-            from.dump(key).then((dump) => {
-                dumpCount++
-                from.pttl(key).then((ttl) => {
-                    ttlCount++
-                    // if the TTL is -1 then there's no TTL, which means 0
-                    // So in Redis universe -1 == 0 :-)
-                    const newttl = ttl === -1 ? 0 : ttl
-                    // Handle other negative values and skip restore
-                    if(newttl >= 0) {
-                        to.restore(key, newttl, dump)
-                        .then((_) => {
-                            restoredCount++
-                        }).catch((err) => {
-                            if(ignore_dupes && err.code === 'BUSYKEY') {
-                                restoredCount++
-                            } else {
-                                restoreErrCount++
-                                console.error(`RESTORE error: ${key}:${ttl} => ${err}`)
-                            }
-                        })
-                        .then(() => {
-                            message = `\x1b[${message.length}D${keyCount} keys`
-                            process.stdout.write(message)
-                        })
-                        .catch() // Dummy catch to satisfy TS compiler
-                    } else {
-                        skippedCount++
-                    }
-                }).catch((err) => {
-                    ttlErrCount++
-                    console.error(`PTTL error: ${key} => ${err}`)
-                })
-            }).catch((err) => {
-                dumpErrCount++
-                console.error(`DUMP error: ${key} => ${err}`)
-            })
-        })
-    } while(cursor !== 0)
-    process.stdout.write('\n')
-    return waitFinish()
+  do {
+    const result: ScanResult = await from.scan(cursor, ['COUNT', batch_size])
+    cursor = parseInt(result[0]);
+    const keys = result[1]
+    keys.forEach(async (key) => {
+      keyCount++
 
-    async function waitFinish() {
-        if(keyCount >= totalKeys && dumpCount + dumpErrCount >= keyCount &&
-           restoredCount + restoreErrCount >= ttlCount &&
-           ttlCount + ttlErrCount + skippedCount >= dumpCount) {
-                console.log(`Total keys: ${keyCount}`)
-                console.log(`Dump errors: ${dumpErrCount}`)
-                console.log(`Restore errors: ${restoreErrCount}`)
-                console.log(`Ttl errors: ${ttlErrCount}`)
-                console.log(`Skipped keys: ${skippedCount}`)
-                const ttime = Date.now() - stime
-                console.log(`Avg speed: ${keyCount * 1000 / ttime} keys/sec`)
-                await from.quit()
-                await to.quit()
+      try {
+        const dump = await from.dump(key)
+        const ttl = await from.pttl(key)
+        readCount++
+        // if the TTL is -1 then there's no TTL, which means 0
+        // So in Redis universe -1 == 0 :-)
+        const newttl = ttl === -1 ? 0 : ttl
+        // Handle other negative values and skip restore
+        if(newttl >= 0) {
+          try {
+            await to.restore(key, newttl, dump)
+            restoredCount++
+          }
+          catch(err) {
+            if(ignore_dupes && err.code === 'BUSYKEY') {
+              restoredCount++
+            } else {
+              restoreErrCount++
+              console.error(`RESTORE error: ${key}:${ttl} => ${err}`)
+              process.exitCode = -1;
+            }
+          }
+          message = `\x1b[${message.length}D${keyCount} keys`
+          process.stdout.write(message)
         } else {
-            setTimeout(waitFinish, 0)
+          // No TTL
+          skippedCount++
         }
+      }
+      catch (err) {
+        readErrCount++
+        console.error(`read error: ${key} => ${err}`)
+        process.exitCode = -1;
+      }
+    })
+  } while(cursor !== 0)
+  process.stdout.write('\n')
+  return waitFinish()
+
+  async function waitFinish() {
+    if(keyCount >= totalKeys && readCount + readErrCount >= keyCount &&
+       restoredCount + restoreErrCount + skippedCount >= readCount ) {
+        console.log(`\nTotal keys: ${keyCount}`)
+        console.log(`Restore errors: ${restoreErrCount}`)
+        console.log(`Read errors: ${readErrCount}`)
+        console.log(`Skipped keys: ${skippedCount}`)
+        const ttime = Date.now() - stime
+        console.log(`Avg speed: ${keyCount * 1000 / ttime} keys/sec`)
+        await from.quit()
+        await to.quit()
+    } else {
+      setTimeout(waitFinish, 0)
     }
+  }
 }
--- src/delete.ts
+++ src/delete.ts
@@ -2,58 +2,58 @@ import * as redis from 'handy-redis'
 
 const redis_url = process.argv[2]
 const pattern = process.argv[3]
-if(!redis_url || redis_url.length === 0 || !pattern || pattern.length === 0) {
-    console.error(`Usage: ${process.argv[1]} redis-url pattern-to-delete`)
-    process.exit(1);
+if (!redis_url || redis_url.length === 0 || !pattern || pattern.length === 0) {
+  console.error(`Usage: ${process.argv[1]} redis-url pattern-to-delete`)
+  process.exit(1);
 }
 
-const red = redis.createHandyClient(redis_url, {return_buffers: true})
+const red = redis.createHandyClient(redis_url, { return_buffers: true })
 
 const batch_size = 10000
 const dry_run = false
 
 list_keys(red)
-    .finally(() => red.quit())
-    .then(() => console.error("Done"))
-    .catch((err) => console.error(err))
+  .finally(() => red.quit())
+  .then(() => console.error("Done"))
+  .catch((err) => console.error(err))
 
 async function list_keys(rediz: redis.IHandyRedis) {
-    
-    let cursor = 0
-    let keyCount = 0
-    let delCount = 0
-    let message = ""
-    const stime = Date.now()
 
-    do {
-        const result = await rediz.scan(
-            cursor,
-            ['MATCH', pattern],
-            ['COUNT', batch_size]
-        )
-        cursor = parseInt(result[0])
-        const keys = result[1] as [string]
-        if(keys.length > 0) {
-            keyCount += keys.length
-            if(dry_run) {
-                delCount += keys.length
-            } else {
-                delCount += await rediz.del.apply(null, keys)
-            }
-            message = `\x1b[${message.length}D${delCount} keys deleted`
-            process.stdout.write(message)
-        }
-    } while(cursor !== 0)
-    process.stdout.write('\n')
-    waitFinish()
+  let cursor = 0
+  let keyCount = 0
+  let delCount = 0
+  let message = ""
+  const stime = Date.now()
 
-    function waitFinish() {
-        if(delCount >= keyCount) {
-                console.error(`Total keys deleted, ${delCount}`)
-                const ttime = Date.now() - stime
-                console.error(`Avg speed, ${delCount * 1000 / ttime}, keys/sec`)
-        } else {
-            setTimeout(waitFinish, 0)
-        }
+  do {
+    const result = await rediz.scan(
+      cursor,
+      ['MATCH', pattern],
+      ['COUNT', batch_size]
+    )
+    cursor = parseInt(result[0])
+    const keys = result[1] as [string]
+    if (keys.length > 0) {
+      keyCount += keys.length
+      if (dry_run) {
+        delCount += keys.length
+      } else {
+        delCount += await rediz.del.apply(null, keys)
+      }
+      message = `\x1b[${message.length}D${delCount} keys deleted`
+      process.stdout.write(message)
     }
+  } while (cursor !== 0)
+  process.stdout.write('\n')
+  waitFinish()
+
+  function waitFinish() {
+    if (delCount >= keyCount) {
+      console.error(`Total keys deleted, ${delCount}`)
+      const ttime = Date.now() - stime
+      console.error(`Avg speed, ${delCount * 1000 / ttime}, keys/sec`)
+    } else {
+      setTimeout(waitFinish, 0)
+    }
+  }
 }
